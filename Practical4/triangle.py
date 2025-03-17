e394f8a43c80772ad717a30f2c21bc98483a6b15
elaine-huang <yunzi.24@intl.zju.edu.cn>
2025-03-17 15:41:37 +0800

    triangle.py

---
+++ Practical4/triangle.py
@@ -0,0 +1,8 @@
+n = 1 # Initialize the "counter" variable n
+triangle_number = 0 # Initialize the desired variable triangle_number
+# Calculate the first ten triangle numbers using a while-loop
+while n <= 10:
+    triangle_number = triangle_number + n
+    print(f"The {n}th triangle number is {triangle_number}.") # Output
+    n += 1 # Counting
+
