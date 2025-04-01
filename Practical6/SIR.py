ae30413452b96040ec88e1b94a978b81f81864a9
elaine-huang <yunzi.24@intl.zju.edu.cn>
2025-04-01 17:22:49 +0800

    move SIR.py into Practical6

--- Practical6/SIR.py
+++ Practical6/SIR.py
@@ -15,15 +15,19 @@ array_for_susceptible = np.array([])
 array_for_infected = np.array([])
 array_for_recovered = np.array([])
 
+plt.figure(figsize=(10,6))
+
 # The time course
-time_points = list(range(1001))
+time_points = range(1001)
 for time_point in time_points: # Loop over 1000 time points
     p_contact = Infected/Population # probability of contact
     p_infected = beta * p_contact
     # Calculate the numbers of newly-recovered and newly-infected people
-    newly_recovered = np.sum(np.random.choice(range(2), Infected, p=[(1-gamma), gamma])) 
-    newly_infected = np.sum(np.random.choice(range(2), Susceptible, p=[(1-p_infected), p_infected]))
-
+    newly_recovered = np.sum(np.random.choice(range(2), Infected, p=[(1-gamma), gamma]))
+    if Susceptible > 0 and p_infected > 0:
+        newly_infected = np.sum(np.random.choice(range(2), Susceptible, p=[(1-p_infected), p_infected]))
+    else:
+        newly_infected = 0
     # Update the numbers of recovered, infected and susceptible people
     Recovered = Recovered + newly_recovered
     Infected = Infected - newly_recovered + newly_infected
@@ -45,5 +49,3 @@ plt.ylabel("number of people")
 plt.title("A simple SIR model")
 plt.legend()
 plt.show()
-
-    
