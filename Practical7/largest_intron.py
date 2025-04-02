5991de1d30e348d1f1327a4eee162b30c1db7f62
elaine-huang <yunzi.24@intl.zju.edu.cn>
2025-04-02 13:14:30 +0800

    edit the pseudocode

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -10,5 +10,6 @@ for intron in introns:
         largest_intron = intron
     else:
         continue
+# Output the largest intron and its length
 print(f"The largest intron that can be possibly generated from this sequence is '{largest_intron}'.")
 print(f"The length of this intron is {largest_length}.")
