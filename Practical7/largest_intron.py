6cdf02c797403fbf9cf2bb72904221419e998bd0
elaine-huang <yunzi.24@intl.zju.edu.cn>
2025-04-02 15:02:46 +0800

    use greedy matching

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -1,15 +1,6 @@
 import re
 seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
-introns = re.findall(r'(?=(GT.+?AG))',seq) # find all possible introns
-largest_length = 0 # initialize the variable
-# Loop through the list of possible introns and find the largest intron
-for intron in introns:
-    length = len(intron)
-    if length > largest_length:
-        largest_length = length
-        largest_intron = intron
-    else:
-        continue
-# Output the largest intron and its length
-print(f"The largest intron that can be possibly generated from this sequence is '{largest_intron}'.")
-print(f"The length of this intron is {largest_length}.")
+largest_intron = re.findall(r'(GT.+AG)',seq) # find the largest possible introns using greedy matching
+# Output the largest possible intron and its length
+print(f"The largest intron that can be possibly generated from this sequence is '{str(largest_intron)[2:-2]}'.")
+print(f"The length of this intron is {len(str(largest_intron)[2:-2])}")
