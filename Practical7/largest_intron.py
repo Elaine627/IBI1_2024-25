d7e1e5b5a187cbd66d4ca28586caf1b6e72a3394
elaine-huang <yunzi.24@intl.zju.edu.cn>
2025-04-08 21:00:58 +0800

    extract the string

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -1,6 +1,7 @@
 import re
 seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
-largest_intron = re.findall(r'(GT.+AG)',seq) # find the largest possible introns using greedy matching
+largest_intron = re.findall(r'(GT.+AG)',seq)[0] # find the largest possible introns using greedy matching
+print(largest_intron)
 # Output the largest possible intron and its length
-print(f"The largest intron that can be possibly generated from this sequence is '{str(largest_intron)[2:-2]}'.")
-print(f"The length of this intron is {len(str(largest_intron)[2:-2])}")
+print(f"The largest intron that can be possibly generated from this sequence is '{largest_intron}'.")
+print(f"The length of this intron is {len(largest_intron)}.")
