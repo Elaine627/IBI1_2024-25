f8cb6c028ccd4be34abc19cb9650c9a37f37d798
elaine-huang <yunzi.24@intl.zju.edu.cn>
2025-05-28 11:37:57 +0800

    revised after feedback

--- Practical5/programming_language_popularity.py
+++ Practical5/programming_language_popularity.py
@@ -6,15 +6,20 @@ print(programming_language_popularity)
 # This part is adapted from lecture 5.1 slides by Professor Chi Ying.
 import numpy as np
 import matplotlib.pyplot as plt
-N = 5
-Users_percentage = (62.3, 52.9, 51, 51, 38.5)
+
+# Get data from dictionary instead of hard-coded values
+languages = list(programming_language_popularity.keys())
+popularity = list(programming_language_popularity.values())
+
+N = len(languages)
 ind = np.arange(N)
 width = 0.4
-p1 = plt.bar(ind, Users_percentage, width)
-plt.ylabel("Users(percentage)")
+p1 = plt.bar(ind, popularity, width)
+plt.xlabel("Programming Languages")
+plt.ylabel("Users (percentage)")
 plt.title("Percentage of developers who use the top 5 programming languages globally as of February 2024")
-plt.xticks(ind,("JavaScript","HTML","Python","SQL","TypeScript"))
-plt.yticks(np.arange(0,101,10))
+plt.xticks(ind, languages)  # Capitalize language names
+plt.yticks(np.arange(0, 101, 10))
 plt.show()
 
 # Return the percentage of developers who use one language taken from the input list
